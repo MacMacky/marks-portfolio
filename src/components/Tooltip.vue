3190f6734a37ace2b2f427c5c1d28ed87d00086f
MacMacky <mac21macky@gmail.com>
2019-07-22 17:12:43 +0800

    Add new helper component 'Tooltip'

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-tooltip bottom :color="tooltipBackground" class="text-xs-center">
+    <template v-slot:activator="{on}">
+      <i v-on="on" class="mb-2">
+        <slot></slot>
+      </i>
+    </template>
+    <span :class="`${tooltipTextColor} text-xs-center d-block`">{{label}}</span>
+    <div class="text-xs-center">
+      <v-progress-circular size="45" color="cyan darken-1" :value="mastery">{{mastery}}%</v-progress-circular>
+    </div>
+  </v-tooltip>
+</template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "TooltipS",
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    mastery: {
+      type: [Number, String],
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters(["tooltipBackground", "tooltipTextColor"])
+  }
+};
+</script>
