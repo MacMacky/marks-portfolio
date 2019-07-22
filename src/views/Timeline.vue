5345f2c1bcb5cf96ebfb11c8bc7105f63b7c096e
MacMacky <mac21macky@gmail.com>
2019-07-22 19:06:45 +0800

    Add new view component 'Timeline'

---
+++ src/views/Timeline.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout justify-center mt-1>
+    <v-flex xs9>
+      <v-timeline>
+        <time-line-item
+          v-for="item in timelineItems"
+          :key="item.color"
+          :color="item.color"
+          :imgSrc="item.src"
+          :date="item.date"
+        ></time-line-item>
+      </v-timeline>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import TimeLineItem from "@/components/TimelineItem.vue";
+import MLLogo from "@/assets/mlhuillier_logo.jpg";
+import CITLogo from "@/assets/school_logo.jpg";
+
+export default {
+  name: "Timeline",
+  components: {
+    TimeLineItem
+  },
+  data() {
+    return {
+      timelineItems: [
+        { color: "red darken-4", src: MLLogo },
+        { color: "#E7C800", src: CITLogo, date: "2017 March" },
+        { src: CITLogo }
+      ]
+    };
+  }
+};
+</script>
