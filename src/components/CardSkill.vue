577b8b1c6783b329076b4f22e26ccb7c580a731d
MacMacky <mac21macky@gmail.com>
2019-07-22 17:12:21 +0800

    Add new helper component 'CardSkill'

---
+++ src/components/CardSkill.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card :color="color" :ripple="isRipple">
+    <v-card-title class="headline text-xs-center d-block white--text">{{title}}</v-card-title>
+    <v-card-text class="text-xs-center">
+      <slot></slot>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "CardSkill",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    },
+    isRipple: {
+      type: Boolean,
+      default: true
+    }
+  }
+};
+</script>
+
