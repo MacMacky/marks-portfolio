7f0a1142024e23fb44ebf46090c2ce5059bb3494
MacMacky <mac21macky@gmail.com>
2019-07-24 17:47:50 +0800

    Add new helper component 'Links'

---
+++ src/components/Links.vue
@@ -0,0 +1,27 @@
+<template>
+  <p class="title mb-1">
+    <v-btn icon title="Linkedin">
+      <linkedin />
+    </v-btn>
+    <v-btn icon title="Github">
+      <github :classes="'link'" />
+    </v-btn>
+    <v-btn icon title="facebook">
+      <facebook />
+    </v-btn>
+  </p>
+</template>
+<script>
+import Github from "@/components/svgs/Github.vue";
+import Facebook from "@/components/svgs/Facebook.vue";
+import Linkedin from "@/components/svgs/Linkedin.vue";
+
+export default {
+  name: "Links",
+  components: {
+    Github,
+    Facebook,
+    Linkedin
+  }
+};
+</script>
