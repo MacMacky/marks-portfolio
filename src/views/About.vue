e533175693737cd53ff8590c70ed25116539a1c8
MacMacky <mac21macky@gmail.com>
2019-07-25 13:45:07 +0800

    Add new view component 'About'

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs4 sm4 md4 lg4>
+        <v-avatar color="grey lighten-4" :size="'256px'">
+          <v-img :src="require('../assets/man.png')" />
+        </v-avatar>
+      </v-flex>
+      <v-flex xs8 sm8 md8 lg8>
+        <p
+          class="subheading text-xs-left"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam soluta quos qui maiores exercitationem, modi culpa sint omnis, quisquam praesentium nulla delectus at, enim corporis. Repudiandae delectus necessitatibus numquam ducimus?</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+<script>
+export default {
+  name: "About"
+};
+</script>
