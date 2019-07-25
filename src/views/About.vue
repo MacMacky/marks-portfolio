e9843d316baa0e949685eac615749354129cd618
MacMacky <mac21macky@gmail.com>
2019-07-25 16:11:10 +0800

    Add new container div with id and classes in 'About' view component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,21 @@
 <template>
-  <v-container>
-    <v-layout row wrap>
-      <v-flex xs12 sm12 md4 lg4 class="text-xs-center">
-        <v-avatar color="grey lighten-4 mb-2" :size="'256px'">
-          <v-img :src="require('../assets/man.png')" />
-        </v-avatar>
-      </v-flex>
-      <v-flex xs12 sm12 md8 lg8>
-        <p
-          class="subheading text-xs-left"
-        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam soluta quos qui maiores exercitationem, modi culpa sint omnis, quisquam praesentium nulla delectus at, enim corporis. Repudiandae delectus necessitatibus numquam ducimus?</p>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div id="about" class="pt-5 mt-3">
+    <h4 class="display-1 text-xs-center mt-4">About</h4>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12 sm12 md4 lg4 class="text-xs-center">
+          <v-avatar color="grey lighten-4 mb-2" :size="'256px'">
+            <v-img :src="require('../assets/man.png')" />
+          </v-avatar>
+        </v-flex>
+        <v-flex xs12 sm12 md8 lg8>
+          <p
+            class="subheading text-xs-left"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam soluta quos qui maiores exercitationem, modi culpa sint omnis, quisquam praesentium nulla delectus at, enim corporis. Repudiandae delectus necessitatibus numquam ducimus?</p>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 <script>
 export default {
