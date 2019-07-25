a2b843cacd9285d02e07586638b66e2a62f6ee0e
MacMacky <mac21macky@gmail.com>
2019-07-25 16:18:06 +0800

    Add new helper component 'SideBarItem'

---
+++ src/components/SideBarItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-list-tile :href="to" :key="label">
+    <v-list-tile-content>
+      <v-list-tile-title class="text-xs-center title">
+        <v-icon class="pr-2">{{icon}}</v-icon>
+        {{label}}
+      </v-list-tile-title>
+    </v-list-tile-content>
+  </v-list-tile>
+</template>
+<script>
+export default {
+  name: "SideBarItem",
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
