7012822ed014faecb9f8adec37250e77907a8a7a
MacMacky <mac21macky@gmail.com>
2019-07-25 17:12:12 +0800

    Updates

---
+++ precache-manifest.47bec3c21f7565afc5d2e3c6ab2fd534.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "933e970411fdfccd1ebfcf1d670f596c",
+    "url": "/img/mlhuillier_logo.933e9704.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "adfa68050830243243f5",
+    "url": "/js/chunk-vendors.8008bbc5.js"
+  },
+  {
+    "revision": "9c080f9c7c2d0ac6be48",
+    "url": "/js/app.300b9695.js"
+  },
+  {
+    "revision": "e6672b38a26a66699a85ca1c05d19575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b821bc7e983614ba358b7de31ae0088e",
+    "url": "/img/school_logo.b821bc7e.jpg"
+  },
+  {
+    "revision": "a5e81f19cf2c587876fd1bb08ae0249f",
+    "url": "/img/man.a5e81f19.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "7f34461a5a2ffd75473444103e2085f0",
+    "url": "/img/js-background.7f34461a.jpeg"
+  },
+  {
+    "revision": "a24ac1204945f057edd55be9e57b99ef",
+    "url": "/img/ezgif.com-gif-maker.a24ac120.gif"
+  },
+  {
+    "revision": "adfa68050830243243f5",
+    "url": "/css/chunk-vendors.95b06b3f.css"
+  },
+  {
+    "revision": "9c080f9c7c2d0ac6be48",
+    "url": "/css/app.cb63de30.css"
+  }
+];
