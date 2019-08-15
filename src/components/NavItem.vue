87857e85fd2d9f4ffa8d094875a1bd686905645e
MacMacky <mac21macky@gmail.com>
2019-08-15 12:35:33 +0800

    Add new component 'NavItem.vue' to separate navitem logic

---
+++ src/components/NavItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn :to="isRouterLink ? to : null" class="nav-menu" flat :href="isRouterLink ? null :to">
+    <span class="mr-2">{{text}}</span>
+  </v-btn>
+</template>
+<script>
+export default {
+  name: "NavItem",
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    isRouterLink: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
