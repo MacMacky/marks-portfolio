b14b5c6e9d0d9c7bd970440004b4a4217309957c
MacMacky <mac21macky@gmail.com>
2019-08-19 10:24:17 +0800

    Add links for social media icons

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,12 +1,17 @@
 <template>
   <p class="title mb-1">
-    <v-btn icon title="Linkedin">
+    <v-btn
+      icon
+      title="Linkedin"
+      href="https://ph.linkedin.com/in/mark-abeto-080644177"
+      target="__blank"
+    >
       <linkedin />
     </v-btn>
-    <v-btn icon title="Github">
+    <v-btn icon title="Github" href="https://github.com/MacMacky" target="__blank">
       <github :classes="'link'" />
     </v-btn>
-    <v-btn icon title="facebook">
+    <v-btn icon title="facebook" href="https://www.facebook.com/Mac2Macky" target="__blank">
       <facebook />
     </v-btn>
   </p>
