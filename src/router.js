cfebed180ec651260afa95a31785d27c79ae43ae
MacMacky <mac21macky@gmail.com>
2019-08-19 10:13:55 +0800

    Remove unused imports

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-//import Home from './views/Home.vue';
-//import Blog from './views/Blog.vue';
-//import Main from './views/Main.vue';
+
 
 Vue.use(Router);
 
